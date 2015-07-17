7880e14967bdf95f31b1c90fc80d10000e3e1e84
martin.grihangne <martin.grihangne@eway.com.au>
2015-07-17 13:31:00 +1000

    Added basic implementation of the AudioMonitor class

---
+++ app/src/main/java/io/martonbot/time/AudioMonitor.java
@@ -0,0 +1,45 @@
+package io.martonbot.time;
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public class AudioMonitor {
+
+    private MediaRecorder mediaRecorder;
+
+    public void startMonitoring() throws IOException {
+
+        if (mediaRecorder == null) {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            // TODO consider changing the output format
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            // TODO consider changing the encoder
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            // TODO consider setting a different bit rate and sampling rate
+            mediaRecorder.setOutputFile("dev/null");
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        }
+
+    }
+
+    public void stopMonitoring() {
+        if (mediaRecorder != null) {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+            mediaRecorder.release();
+        }
+    }
+
+    public int getMaxAmplitude() {
+        if (mediaRecorder != null) {
+            return mediaRecorder.getMaxAmplitude();
+        }
+        return 0;
+    }
+
+
+
+}
